d9c25246797df19c530c23bc79f4a720dcae76a1
Ricardo Chejfec Ciociano <ricardo.chejfec@mail.mcgill.ca>
2018-08-08 14:13:39 -0400

    project skeleton

--- PyPDFTracker.py
+++ PyPDFTracker.py
@@ -1,8 +1,9 @@
 """Script to create a report of PDF's created (scanned) by day"""
 
 import sys
-import os 
-import json
+import os
+import platform
+import time
 from pyPdf import PdfFileReader
 
 directory = "C:/Users/rchejf/Documents/Scanned Documents/"
@@ -19,6 +20,15 @@ def create_master_dictionary(list_of_files):
 	first document of the day
 	last document of the day 
 	"""
+    
+    for filename in os.listdir(directory):
+	master_dict = {}
+        filename_full = directory + filename
+	date_created_full = time.ctime(os.path.getctime(filename_full)).split(" ")
+	date_created = " ".join([date_created_full[i] for i in [0,2,1,4]])
+	time_created = date_created_full[3]
+	 
+	
     pass
 
 def calculate_totals(master_dict):
