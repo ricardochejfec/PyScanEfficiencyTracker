8a74d0ff3f86719772cc3503e4c304159f77bb1f
Ricardo Chejfec Ciociano <ricardo.chejfec@mail.mcgill.ca>
2018-08-08 12:35:31 -0400

    project skeleton

---
+++ PyPDFTracker.py
@@ -0,0 +1,9 @@
+"""Script to create a report of PDF's created (scanned) by day"""
+import sys
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
