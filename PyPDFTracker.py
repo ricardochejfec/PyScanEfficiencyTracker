6841295aa9670022329c447b947962694ce54eaf
Ricardo Chejfec Ciociano <ricardo.chejfec@mail.mcgill.ca>
2018-08-08 12:50:14 -0400

    project skeleton

--- PyPDFTracker.py
+++ PyPDFTracker.py
@@ -1,9 +1,39 @@
 """Script to create a report of PDF's created (scanned) by day"""
+
 import sys
+import os 
+import json
+from pyPdf import PdfFileReader
+
+directory = "C:/Users/rchejf/Documents/Scanned Documents/"
 
 def main():
     """Main entry point for the script."""
     pass
+
+
+def create_master_dictionary(list_of_files):
+    """Iterate through all files and create a dictionary containing by date: 
+	# of documents,
+	# of pages,
+	first document of the day
+	last document of the day 
+	"""
+    pass
+
+def calculate_totals(master_dict):
+    """Calculate:
+	- total hours 
+	- total documents
+	- total pages 
+	- hours per day
+	- efficiency per day"""
+    pass
+
+def create_report(master_dict): 
+     """create a report in markdown into an html file and open it"""
+    pass
+
 
 if __name__ == '__main__':
     sys.exit(main())
